d6c4c660de2b4089e6fbeaf920aa00f899ce81af
chan-shiro <shiro.fkd@gmail.com>
2025-07-15 22:53:03 +0900

    何指定したらいいんだ謎

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,14 +1,8 @@
-import path from 'path';
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   test: {
     environment: 'node',
     printConsoleTrace: true,
